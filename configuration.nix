c1cb0abb1b9c8b4a36de50dcb51acf424f053973
Artur Motyka <artur@motyka.pro>
2025-09-11 00:32:14 +0200

    Init

---
+++ configuration.nix
@@ -0,0 +1,162 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, hardware, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+       <nixos-hardware/framework/13-inch/7040-amd>
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  boot.initrd.luks.devices."luks-baccf639-6b86-41fe-8ec3-a2ecb815b6a1".device = "/dev/disk/by-uuid/baccf639-6b86-41fe-8ec3-a2ecb815b6a1";
+  networking.hostName = "ramapraca";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Warsaw";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pl_PL.UTF-8";
+    LC_IDENTIFICATION = "pl_PL.UTF-8";
+    LC_MEASUREMENT = "pl_PL.UTF-8";
+    LC_MONETARY = "pl_PL.UTF-8";
+    LC_NAME = "pl_PL.UTF-8";
+    LC_NUMERIC = "pl_PL.UTF-8";
+    LC_PAPER = "pl_PL.UTF-8";
+    LC_TELEPHONE = "pl_PL.UTF-8";
+    LC_TIME = "pl_PL.UTF-8";
+  };
+
+  virtualisation.docker.enable = true;
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = false;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+  };
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "pl";
+    variant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "pl2";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Flatpak
+  services.flatpak.enable = true;
+
+  # VPN
+  # Obejscie na gazomierzu zajebane z https://github.com/ncaq/dotfiles/blob/e628194155fdba694c9b76242a718024e973b857/nixos/host/seminar/vpn.nix
+  services.tailscale = {
+    enable = true;
+    useRoutingFeatures = "server"; # Exit Nodeとして動作
+    # [tailscale: Build failure with portlist tests on NixOS 25.05 - "seek /proc/net/tcp: illegal seek" · Issue #438765 · NixOS/nixpkgs](https://github.com/nixos/nixpkgs/issues/438765)
+    package = pkgs.tailscale.overrideAttrs (old: {
+      checkFlags = builtins.map (
+        flag:
+        if lib.hasPrefix "-skip=" flag then
+          flag + "|^TestGetList$|^TestIgnoreLocallyBoundPorts$|^TestPoller$"
+        else
+          flag
+      ) old.checkFlags;
+    });
+  };
+
+  # Steam
+  programs.steam.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.artur9010 = {
+    isNormalUser = true;
+    description = "Artur Motyka";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    packages = with pkgs; [
+      # k8s
+      pkgs.kubectl
+      pkgs.kubernetes-helm
+      pkgs.awscli
+      pkgs.sops
+      #
+      pkgs.floorp
+      pkgs.vscode
+      pkgs.vlc
+      pkgs.thunderbird
+      # gierkowanie
+      pkgs.prismlauncher
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    wget
+    btop-rocm
+    git
+    dua
+    pkgs.ryzenadj
+    pkgs.lm_sensors
+  ];
+  
+  # Ryzenadj requirement
+  #hardware.cpu.amd.ryzen-smu.enable = true;
+  boot.extraModulePackages = [
+    (config.boot.kernelPackages.callPackage /etc/nixos/pkgs/ryzen_smu { })
+  ];
+
+  programs.bash.shellAliases = {
+    ncdu = "dua i";
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
