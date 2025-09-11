d78f4e21499f6a0d42f0e591c00c96d965ac000f
Artur Motyka <artur@motyka.pro>
2025-09-11 20:03:34 +0200

    add ozone envvars to fix scaling in electron apps

--- desktop.nix
+++ desktop.nix
@@ -12,4 +12,7 @@
     pkgs.kdePackages.elisa
     pkgs.kdePackages.kate
   ];
+
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.sessionVariables.ELECTRON_OZONE_PLATFORM_HINT = "wayland";
 }
