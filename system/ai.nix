76f1e0b0352a87f59ed797c2b7eab279756bb301
Artur Motyka <artur@motyka.pro>
2026-01-14 02:01:19 +0100

    remove copilot-cli

--- system/ai.nix
+++ system/ai.nix
@@ -1,25 +1,6 @@
 { config, pkgs, ... }:
 
 let
-  copilotSandboxed = pkgs.writeShellScriptBin "copilot" ''
-    # ProtectHome=read-only, przy yes nie odpala sie nic jak workdir jest wewnatrz /home
-    # .cache/nix potrzebne do dzialania nix-shell wewnatrz sandboxa
-    exec systemd-run --user --pty \
-      --unit=copilot-sandboxed-$RANDOM \
-      --working-directory="$PWD" \
-      --setenv=PATH="$PATH" \
-      -p ProtectHome=read-only \
-      -p PrivateTmp=yes \
-      -p ProtectSystem=strict \
-      -p NoNewPrivileges=yes \
-      -p RestrictSUIDSGID=yes \
-      -p PrivateDevices=yes \
-      -p ReadWritePaths="$PWD" \
-      -p ReadWritePaths="$HOME/.copilot" \
-      -p ReadWritePaths="$HOME/.cache/nix" \
-      ${pkgs.github-copilot-cli}/bin/copilot "$@"
-  '';
-
   claudeSandboxed = pkgs.writeShellScriptBin "claude" ''
     # ProtectHome=read-only, przy yes nie odpala sie nic jak workdir jest wewnatrz /home
     # .cache/nix potrzebne do dzialania nix-shell wewnatrz sandboxa
@@ -44,8 +25,6 @@ let
   '';
 in {
   environment.systemPackages = [
-    copilotSandboxed
-    #
     claudeSandboxed
     pkgs.claude-monitor
     # lsp: https://opencode.ai/docs/lsp/
