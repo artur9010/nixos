29ee24650a99a6282542b0c9e404e82bdb015785
Artur Motyka <artur@motyka.pro>
2026-03-04 18:10:23 +0100

    remove flatpak fonts workaround which broke flatpak apps

--- systems/ramapraca/flatpak.nix
+++ systems/ramapraca/flatpak.nix
@@ -29,40 +29,4 @@
       "org.gimp.GIMP"
     ];
   };
-
-  # https://wiki.nixos.org/wiki/Fonts#Solution_3:_Configure_bindfs_for_fonts/cursors/icons_support
-  # Expose fonts and cursors on /usr/share/{fonts,icons}, so flatpak can discover and use them.
-  system.fsPackages = [ pkgs.bindfs ];
-
-  fileSystems =
-    let
-      mkRoSymBind = path: {
-        device = path;
-        fsType = "fuse.bindfs";
-        options = [
-          "ro"
-          "resolve-symlinks"
-          "x-gvfs-hide"
-        ];
-      };
-      aggregated = pkgs.buildEnv {
-        name = "system-fonts-and-icons";
-        paths =
-          config.fonts.packages
-          ++ (with pkgs; [
-            # Add your cursor themes and icon packages here
-            # bibata-cursors
-            # gnome.gnome-themes-extra
-            # etc.
-          ]);
-        pathsToLink = [
-          "/share/fonts"
-          "/share/icons"
-        ];
-      };
-    in
-    {
-      "/usr/share/fonts" = mkRoSymBind "${aggregated}/share/fonts";
-      "/usr/share/icons" = mkRoSymBind "${aggregated}/share/icons";
-    };
 }
